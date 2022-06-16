9e2b7aa77efeef4d5d014f3bbc6bfca4b57de4e0
Nicolas Yacono <nmyacono@gmail.com>
2022-06-16 19:40:39 -0300

    Alogaritmo simple con alert y consol.log creado

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 let nombre = prompt("Ingrese su nombre");
 let apellido = prompt("Ingrese su apellido");
 alert("Bienvenido " + nombre + " " + apellido);
+let x = 20;
+let y = "20";
+let z = "Estamos en el"
+console.log(z + " " + x + y);
